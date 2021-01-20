187dc79d229a1830dcd89ec5c0539c6b08a7d801
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-01-20 18:05:25 +0530

    add

---
+++ edit_distance.cpp
@@ -0,0 +1,108 @@
+// Template begins
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stdio.h>
+#include <math.h>
+#include <iomanip>
+#include <queue> 
+#include <string.h>
+#include <string>
+using namespace std;
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+#define fo(i,a,b) for(i=a;i<=b;i++)
+#define all(v) (v).begin(),(v).end()
+#define sort0(v) sort(all(v))
+#define lbb(a,b,arr,x) lower_bound(arr+a,arr+b+1,x)-arr
+#define ubb(a,b,arr,x) upper_bound(arr+a,arr+b+1,x)-arr
+#define freq(a,b,arr,x) upper_bound(arr+a,arr+b+1,x)-lower_bound(arr+a,arr+b+1,x)
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define d0(x) cout<<(x)<<" "
+#define d1(x) cout<<(x)<<endl
+#define d2(x,y) cout<<(x)<<" "<<(y)<<endl
+#define d3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl
+#define d4(a,b,c,d) cout<<(a)<<" "<<(b)<<" "<<(c)<<" "<<(d)<<endl
+#define d5(a,b,c,d,e) cout<<(a)<<" "<<(b)<<" "<<(c)<<" "<<(d)<<" "<<(e)<<endl
+#define d6(a,b,c,d,e,f) cout<<(a)<<" "<<(b)<<" "<<(c)<<" "<<(d)<<" "<<(e)<<" "<<(f)<<endl
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define ss second
+#define ff first
+#define pb push_back
+#define mp make_pair
+#define printp(x) cout << x.ff << " " << x.ss << endl
+ 
+const ll mod=998244353;
+#define inf 9999999999999
+#define MAXN   100001 
+  
+// stores smallest prime factor for every number 
+ 
+ 
+ll inv(ll i){if(i==1) return 1;return (mod-((mod/i)*inv(mod%i))%mod)%mod;}
+ 
+ll gcd(ll a,ll b){if(a==0) return b;return gcd(b,b%a);}
+inline void fastio(){ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);}
+// Template ends
+const int MN = 2e5+7;
+int editDistance(string s1, string s2){
+
+  /*  Don't write main().
+   *  Don't read input, it is passed as function argument.
+   *  Return output and don't print it.
+   *  Taking input and printing output is handled automatically.
+   */
+	int m = s1.size();
+	int n = s2.size();
+
+	vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
+
+	for (int i = 0; i < m+1; ++i)
+	{
+		dp[i][0] = i;
+	}
+
+
+	for (int i = 0; i < n+1; ++i)
+	{
+		dp[0][i] = i;
+	}
+
+	//dp[m][n] = 0;
+
+	for (int i = 1; i <= m; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if(s1[i-1]==s2[j-1]){
+				dp[i][j] = dp[i-1][j-1];
+			}else{
+
+				dp[i][j] = min(1+dp[i-1][j-1], min(1+dp[i-1][j], 1+dp[i][j-1]));
+			}
+		}
+	}
+
+	
+
+	return dp[m][n];
+
+
+}
+void solve(){
+    string a,b; cin>>a>>b;
+    cout<<editDistance(a,b)<<endl;
+}
+int main(){
+    fastio();
+    //write code
+    solve();
+    return 0;
+}
